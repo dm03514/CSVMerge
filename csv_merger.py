fc5f8be93abaf8c3e3d38f0a896dafc7b8b00e4b
Daniel Mican <dm03514@dmmba.(none)>
2011-08-21 21:38:30 -0400

    v .01 of CSVMerger, Lets see how well it does with 30, 40,000 line csvs!

---
+++ csv_merger.py
@@ -0,0 +1,45 @@
+from csv import DictReader, writer
+from tempfile import TemporaryFile
+
+
+def merge_csvs(csv_to_merge_list):
+  """
+  Takes a list of csv filepaths to merge.  Saves a combined file in the current directory.
+  """
+  dict_reader_list = []
+  headings = None
+  for csv_dir in csv_to_merge_list:
+    try:
+      f = open(csv_dir, 'r')
+    except IOError:
+      print 'Error opening up file %s' % (csv_dir)
+      return
+
+    reader = DictReader(f)
+
+    if not headings:
+      headings = reader.fieldnames
+    else:
+      # verify fieldnames match up.
+      if headings != reader.fieldnames:
+        print 'Fields do not match up' 
+        return
+
+    dict_reader_list.append(reader)
+
+  # create a new file
+  with TemporaryFile() as temp_file:
+    csv_writer = writer(temp_file)
+    for reader in dict_reader_list:
+      csv_writer.writerows([line_dict.values() for line_dict in reader])
+
+    #import ipdb; ipdb.set_trace() 
+    temp_file.seek(0)
+    perm_file = open('perm.csv', 'w')
+    perm_file.write('%s\r\n' % (','.join(headings)))
+    perm_file.writelines(line for line in temp_file)
+    perm_file.close()
+
+
+merge_csvs(['test.csv', 'test1.csv'])
+
