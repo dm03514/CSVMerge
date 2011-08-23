938c47899b8af011a3d45aecf799e33d094e63a3
Daniel Mican <dm03514@dmmba.local>
2011-08-23 19:53:51 -0400

    Took out temp file, now we only write 1x to the perm_file. Closes #1.

--- csv_merger.py
+++ csv_merger.py
@@ -1,6 +1,5 @@
 from csv import DictReader, writer
 from datetime import date
-from tempfile import TemporaryFile
 
 
 def merge_csvs(csv_to_merge_list):
@@ -25,22 +24,16 @@ def merge_csvs(csv_to_merge_list):
       if headings != reader.fieldnames:
         print 'Fields do not match up' 
         return
-
     dict_reader_list.append(reader)
 
   # create a new file
-  with TemporaryFile() as temp_file:
-    csv_writer = writer(temp_file)
+  with open('combined_%s.csv' % (date.today()), 'w') as perm_file:
+    csv_writer = writer(perm_file)
+    csv_writer.writerow(headings)
     for reader in dict_reader_list:
       csv_writer.writerows(line_dict.values() for line_dict in reader)
-
-    #import ipdb; ipdb.set_trace() 
-    temp_file.seek(0)
-    perm_file = open('combined_%s.csv' % (date.today()), 'w')
-    perm_file.write('%s\r\n' % (','.join(headings)))
-    perm_file.writelines(line for line in temp_file)
     perm_file.close()
+    #import ipdb; ipdb.set_trace() 
 
-
-merge_csvs(['test.csv', 'test1.csv'])
+merge_csvs(['50onred.csv', 'Firstand10.csv', 'Commission Junction.csv', 'WorkingPlanet.csv', 'test.csv', 'test1.csv', 'test2.csv', 'test3.csv'])
 
